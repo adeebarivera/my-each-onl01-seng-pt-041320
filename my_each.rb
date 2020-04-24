dea3b6a1257e0246f12d0d46b6014d726fb7b53f
adeeba reaz <adeebarivera@outlook.com>
2020-04-24 02:05:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
-  # code here
-end
+# collection = [1, 2, 3, 4]
+# def my_each(collection) 
+#   do |i|
+#   puts i
+# end
+
+# def my_each(collection)
+#   i = 0
+#   while i > collection.length
+#   yield(collection[i]) 
+#   i = i + 1
+# end
+# end
+
+
+
+
+
+# def my_each # put argument(s) here
+#   # code here
+# end
